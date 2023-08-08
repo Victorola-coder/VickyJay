9fcdf01cdc0b875899a8c70954af4e85981ebaaa
Victor Olatunji VickyJay <victoluolatunji@gmail.com>
2023-08-08 03:34:44 +0100

    --navbar init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <>
+      <div>
+        <header>
+          <nav>
+            <h2 cla>VickyJay<span>.</span></h2>
+          </nav>
+        </header>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
