2ce9ec8ea97bd9abc9450006821261f3d6a4e151
Victor Olatunji VickyJay <victoluolatunji@gmail.com>
2023-08-08 19:35:02 +0100

    scroll? no

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -4,7 +4,7 @@ import errorImg from "../assets/images/favicon.png";
 const ErrorPage = () => {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center w-full h-screen gap-7">
+      <div className="flex flex-col items-center justify-center w-full gap-7">
         <h1 className="text-[40px] leading-[25.78px] text-transparent h-12  bg-clip-text bg-gradient-to-r from-[#1ED760] to-[#069447] font-clash font-bold md:text-[40px] md:leading-[30.47px]">
          VickyJay
         </h1>
