01fe32688fcc2aa6bb7baf7c2bd3454b7f242456
Victor Olatunji VickyJay <victoluolatunji@gmail.com>
2023-08-08 18:04:15 +0100

    add: error 4040 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
+import ErrorPage from "./Pages/ErrorPage";
 const App = () => {
   return (
     <Routes>
-      <Route index path="/" element={<Home />}></Route>
+      <Route index path="/" element={<Home />}/>
+      <Route index path="*" element={<ErrorPage />}/>
     </Routes>
   );
 };
