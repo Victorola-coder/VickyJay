d21b363d10209710dd00d6977adc305add249f72
Victor Olatunji VickyJay <victoluolatunji@gmail.com>
2023-08-13 22:04:40 +0100

    hunger

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -17,10 +17,10 @@ const Services = () => {
       {/* cards */}
       <div className="flex flex-col mt-[56px] gap-2 lg:mt-[56px] lg:flex lg:flex-wrap lg:flex-shrink-0 lg:justify-center lg:flex-row lg:gap-[24px]">
         <div
-          className={`flex flex-col gap-3 flex-nowrap justify-start md:flex md:flex-row md:flex-wrap md:justify-around md:gap-4 lg:flex lg:flex-col lg:gap-[24px]`}
+          className={`flex flex-col gap-3 flex-nowrap justify-start md:flex md:flex-row md:flex-wrap md:justify-around md:gap-4 lg:flex lg:flex-col lg:gap-[0px]`}
         >
           <div
-            className={`bg-[#0A1C20] text-[#FFFFFF99] py-[65px] px-[27px] max-w-[369.808px] h-[240px] lg:py-[100px] lg:px-[49px] lg:w-[641px] lg:h-[416px] shrink-0`}
+            className={`bg-[#0A1C20] text-[#FFFFFF99] py-[65px] px-[27px] max-w-[369.808px] h-[240px] lg:py-[100px] lg:px-[49px] lg:max-w-[641px] lg:h-[416px] shrink-0`}
           >
             <h4
               className={`font-semibold text-white font-clashbold text-[20px] leading-[24.6px] max-w-[156px] lg:max-w-[312px] lg:text-[40px] lg:leading-[49.2px]`}
@@ -34,9 +34,9 @@ const Services = () => {
               Bootstrap, JavaScript, ReactJs, NextJs & Typescript.
             </p>
           </div>
-          <div className="bg-gradient-to-r max-w-[369.808px] lg:w-[642px] to-[#F0FEFF4D] from-[#F0FEFF00] pt-0.5">
+          <div className="bg-gradient-to-r max-w-[369.808px] lg:max-w-[642px] to-[#F0FEFF4D] from-[#F0FEFF00] pt-0.5">
             <div
-              className={`bg-[#0A1C20] shrink-0 text-[#FFFFFF99] py-[65px] px-[27px] max-w-[369.808px] h-[240px] lg:py-[100px] lg:px-[49px] lg:w-[641px] lg:h-[416px]`}
+              className={`bg-[#0A1C20] shrink-0 text-[#FFFFFF99] py-[65px] px-[27px] max-w-[369.808px] h-[240px] lg:py-[100px] lg:px-[49px] lg:max-w-[641px] lg:h-[416px]`}
             >
               <h4
                 className={`font-semibold uppercase text-white text-[20px] leading-[24.6px] font-clashbold max-w-[156px] lg:max-w-[400px] lg:text-[40px] lg:leading-[49.2px]`}
@@ -52,7 +52,7 @@ const Services = () => {
             </div>
           </div>
           <div
-            className={`bg-[#0A1C20] text-[#FFFFFF99] py-[65px] px-[27px] max-w-[369.808px] h-[240px] lg:py-[100px] lg:px-[49px] lg:w-[641px] lg:h-[416px] shrink-0`}
+            className={`bg-[#0A1C20] text-[#FFFFFF99] py-[65px] px-[27px] max-w-[369.808px] h-[240px] lg:py-[100px] lg:px-[49px] lg:max-w-[641px] lg:h-[416px] shrink-0`}
           >
             <h4
               className={`font-semibold uppercase text-[20px] leading-[24.6px] max-w-[156px] text-white font-clashbold lg:max-w-[312px] lg:text-[40px] lg:leading-[49.2px]`}
@@ -67,7 +67,7 @@ const Services = () => {
             </p>
           </div>
           <div
-            className={`bg-[#0A1C20] text-[#FFFFFF99] py-[65px] px-[27px] max-w-[369.808px] h-[240px] lg:py-[100px] lg:px-[49px] lg:w-[641px] lg:h-[416px] shrink-0`}
+            className={`bg-[#0A1C20] text-[#FFFFFF99] py-[65px] px-[27px] max-w-[369.808px] h-[240px] lg:py-[100px] lg:px-[49px] lg:max-w-[641px] lg:h-[416px] shrink-0`}
           >
             <h4
               className={`font-semibold uppercase text-[20px] leading-[24.6px] max-w-[156px] text-white font-clashbold lg:max-w-[312px] lg:text-[40px] lg:leading-[49.2px]`}
@@ -85,11 +85,11 @@ const Services = () => {
         </div>
         {/* second card */}
         <div
-          className={`mt-[36px] flex flex-row gap-4 lg:mt-[148px] lg:flex lg:flex-col lg:gap-[24px]`}
+          className={`flex flex-col gap-3 flex-nowrap justify-start md:flex md:flex-row md:flex-wrap md:justify-around md:gap-4 lg:mt-[148px] lg:flex lg:flex-col lg:gap-[24px]`}
         >
           <div className="bg-gradient-to-l max-w-max to-[#F0FEFF4D] from-[#F0FEFF00] pl-0.5 pt-0.5">
             <div
-              className={`bg-[#0A1C20] text-[#FFFFFF99] lg:py-[100px] lg:px-[49px] lg:w-[641px] lg:h-[416px] shrink-0`}
+              className={`bg-[#0A1C20] text-[#FFFFFF99] py-[65px] px-[27px] max-w-[369.808px] h-[240px] lg:py-[100px] lg:px-[49px] lg:max-w-[641px] lg:h-[416px] shrink-0`}
             >
               <h4
                 className={`font-semibold text-white font-clashbold lg:max-w-[312px] lg:text-[40px] lg:leading-[49.2px]`}
@@ -105,7 +105,7 @@ const Services = () => {
             </div>
           </div>
           <div
-            className={`bg-[#0A1C20] shrink-0 text-[#FFFFFF99] lg:py-[100px] lg:px-[49px] lg:w-[641px] lg:h-[416px]`}
+            className={`bg-[#0A1C20] shrink-0 text-[#FFFFFF99] lg:py-[100px] lg:px-[49px] lg:max-w-[641px] lg:h-[416px]`}
           >
             <h4
               className={`font-semibold uppercase text-white font-clashbold lg:max-w-[400px] lg:text-[40px] lg:leading-[49.2px]`}
@@ -121,7 +121,7 @@ const Services = () => {
             </p>
           </div>
           <div
-            className={`bg-[#0A1C20] text-[#FFFFFF99] lg:py-[100px] lg:px-[49px] lg:w-[641px] lg:h-[416px] shrink-0`}
+            className={`bg-[#0A1C20] text-[#FFFFFF99] lg:py-[100px] lg:px-[49px] lg:max-w-[641px] lg:h-[416px] shrink-0`}
           >
             <h4
               className={`font-semibold uppercase text-white font-clashbold lg:max-w-[312px] lg:text-[40px] lg:leading-[49.2px]`}
@@ -136,7 +136,7 @@ const Services = () => {
             </p>
           </div>
           <div
-            className={`bg-[#0A1C20] text-[#FFFFFF99] lg:py-[100px] lg:px-[49px] lg:w-[641px] lg:h-[416px] shrink-0`}
+            className={`bg-[#0A1C20] text-[#FFFFFF99] lg:py-[100px] lg:px-[49px] lg:max-w-[641px] lg:h-[416px] shrink-0`}
           >
             <h4
               className={`font-semibold uppercase text-white font-clashbold lg:max-w-[312px] lg:text-[40px] lg:leading-[49.2px]`}
