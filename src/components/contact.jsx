533870c684045fc7ed0f4de6cddb61a70f705419
Victor Olatunji VickyJay <victoluolatunji@gmail.com>
2023-08-16 12:17:50 +0100

    contact section init

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,3 +1,5 @@
+import { LocationIcon, MailIcon, PhoneIcon } from "../assets/Icons";
+
 const Contact = () => {
   return (
     <section className="mx-[30px] mt-[60px] lg:mx-[76px] lg:mt-[150px]">
@@ -17,7 +19,45 @@ const Contact = () => {
         </p>
       </div>
       <div className={`mt-[51px] flex flex-wrap items-center justify-between`}>
-        <div></div>
+        <div className="flex flex-col items-center gap-[10px]">
+          <div className="bg-gradient-to-l from-[#CFFAFF] to-[#CFFAFF00] pr-[1px] pt-[1px] max-w-[75px] max-h-[75px]">
+            <MailIcon
+              className={`p-[5px] bg-[#061417] md:w-[36px] md:h-[36px]`}
+            />
+          </div>
+          <a
+            className={`text-[#FFFFFF99] font-medium md:text-[20px] md:leading-[30px]`}
+            href="mailto:victoluolatunji@gmail.com"
+          >
+            victoluolatunji@gmail.com
+          </a>
+        </div>
+        <div className="flex flex-col items-center gap-[10px]">
+          <div className="bg-gradient-to-r to-[#CFFAFF] from-[#CFFAFF00] pr-[1px] pt-[1px] max-w-[75px] max-h-[50px]">
+            <PhoneIcon
+              className={`p-[5px] bg-[#061417] md:w-[36px] md:h-[36px]`}
+            />
+          </div>
+          <a
+            href="tel:+2349075789680"
+            className={`text-[#FFFFFF99] font-medium md:text-[20px] md:leading-[30px]`}
+          >
+            it is not necessary tho
+          </a>
+        </div>
+        <div className="flex flex-col items-center gap-[10px]">
+          <div className="bg-gradient-to-l from-[#CFFAFF] to-[#CFFAFF00] pr-[1px] pt-[1px] max-w-[75px] max-h-[75px]">
+            <LocationIcon
+              className={`p-[5px] bg-[#061417] md:w-[36px] md:h-[36px]`}
+            />
+          </div>
+          <p
+            className={`text-[#FFFFFF99] font-medium md:text-[20px] md:leading-[30px]`}
+          >
+            somewhere in the metaverse
+          </p>
+          <a href="tel:+"></a>
+        </div>
       </div>
     </section>
   );
