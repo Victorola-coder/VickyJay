6390cef253259a76afef387a3c556940fe32d797
Victor Olatunji VickyJay <victoluolatunji@gmail.com>
2023-08-21 03:33:05 +0100

    --demmo-V1.0.0

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,8 +1,56 @@
 export const projectData = [
     {
         name: 'text2Speech',
-        desc: ' converts the text input to an audio output',
-        lang: ' JavaScript + webspeechAPI + TailwindCss + Html',
+        desc: 'converts the text input to an audio output',
+        lang: 'JavaScript + webspeechAPI + TailwindCss + Html',
         link: 'https://text2speeech.vercel.app/',
+    },
+    {
+        name: 'hbdAoPresh',
+        desc: 'Happpu Birthday Wish to Ao Presh',
+        lang: ' JavaScript + CSS + Html',
+        link: 'https://hbd-aopresh.vercel.app',
+    },
+    {
+        name: 'askSolomon',
+        desc: 'A search engine prototype that wokrs like google',
+        lang: 'JavaScript + Css + Html',
+        link: 'https://asksolomon.vercel.app/',
+    },
+    {
+        name: 'Zeddpay(clone)',
+        desc: 'zeddpay V1 web clone',
+        lang: ' JavaScript + Css + Html',
+        link: 'https://zeddpay-clvne.vercel.app/',
+    },
+    {
+        name: 'Scrimm',
+        desc: 'Get to know everything about scrim.',
+        lang: 'Bootstrap + Html',
+        link: 'hhttps://victorola-coder.github.io/scrimm/',
+    },
+    {
+        name: 'afrifanz',
+        desc: 'only fanz for africans content creators',
+        lang: 'JavaScript + ReactJS + TailwindCss',
+        link: 'https://waitlist.afrifnaz.com/',
+    },
+    {
+        name: 'showmeelove',
+        desc: 'buymeeacoffe for african creators',
+        lang: 'ReactJs + Tailwindcs',
+        link: 'https://showmeelove.com/',
+    },
+    {
+        name: 'luxstay',
+        desc: 'help the homeless get affordable house',
+        lang: 'ReactJs + TailwindCss',
+        link: 'https://luxstayy.vercel.app/',
+    },
+    {
+        name: 'TekArt',
+        desc: 'bridging the gap between the tech and art space',
+        lang: 'ReactJs + TailwindCss',
+        link: 'https://tekarthq.com/',
     },
 ]
