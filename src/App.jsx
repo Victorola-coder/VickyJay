bc0e28d33a4dae5a2d58264bd35ad5f5e49a9623
Victor Olatunji <victoluolatunji@gmail.com>
2023-09-15 20:15:59 +0100

    chore: trying to add animations, will figure it out later

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import ErrorPage from "./Pages/ErrorPage";
 import Test from "./Pages/test";
+import { useEffect } from "react";
+import Scroll from "./animation/scroll";
+
 const App = () => {
+  useEffect(() => {
+    Scroll();
+  }, []);
+
   return (
     <main className={`transition-all duration-300`}>
       <Routes>
---
+++ src/animation/scroll.js
@@ -0,0 +1,31 @@
+import gsap from "gsap";
+import Lenis from "@studio-freight/lenis";
+
+export default function Scroll() {
+  const lenis = new Lenis({
+    duration: 1.6,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    infinite: false,
+    smooth: true,
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+}
+
+// === i might have to use this later
+// const lenis = new Lenis({
+//   lerp: 0.1,
+// });
+
+// lenis.on("scroll", ScrollTrigger.update);
+
+// gsap.ticker.add(time => {
+//   lenis.raf(time * 1000);
+// });
+
+// gsap.ticker.lagSmoothing(0);
