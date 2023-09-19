f42906194a514baf38fc85f12e3684b3051e0ccc
Victor Olatunji <victoluolatunji@gmail.com>
2023-09-19 01:33:41 +0100

    Revert "fix: typo in project.js"
    This reverts commit 87307545caef4af4bb1f07baa07c5a0162384e56.

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,56 +1,56 @@
 export const projectData = [
-  {
-    name: "text2Speech",
-    desc: "converts the text input to an audio output",
-    lang: "JavaScript + webSpeechAPI + TailwindCss + Html",
-    link: "https://text2speeech.vercel.app/",
-  },
-  {
-    name: "hbdAoPresh",
-    desc: "Happy Birthday Wish to Ao Presh",
-    lang: " JavaScript + CSS + Html",
-    link: "https://hbd-aopresh.vercel.app",
-  },
-  {
-    name: "askSolomon",
-    desc: "A search engine prototype that works like google",
-    lang: "JavaScript + Css + Html",
-    link: "https://asksolomon.vercel.app/",
-  },
-  {
-    name: "Zeddpay(clone)",
-    desc: "zeddpay V1 web clone",
-    lang: " JavaScript + Css + Html",
-    link: "https://zeddpay-clvne.vercel.app/",
-  },
-  {
-    name: "Scrimm",
-    desc: "Get to know everything about scrim.",
-    lang: "Bootstrap + Html",
-    link: "https://victorola-coder.github.io/scrimm/",
-  },
-  {
-    name: "afrifanz",
-    desc: "onlyFans for africans content creators",
-    lang: "JavaScript + ReactJS + TailwindCss",
-    link: "https://waitlist.afrifanz.com/",
-  },
-  {
-    name: "showmeelove",
-    desc: "buymeeacoffe for african creators",
-    lang: "ReactJs + Tailwindcss",
-    link: "https://showmeelove.com/",
-  },
-  {
-    name: "luxstay",
-    desc: "not the common accommodation website",
-    lang: "ReactJs + TailwindCss",
-    link: "https://luxstayy.vercel.app/",
-  },
-  {
-    name: "TekArt",
-    desc: "bridging the gap between the tech and art space",
-    lang: "ReactJs + TailwindCss",
-    link: "https://tekarthq.com/",
-  },
-];
+    {
+        name: 'text2Speech',
+        desc: 'converts the text input to an audio output',
+        lang: 'JavaScript + webspeechAPI + TailwindCss + Html',
+        link: 'https://text2speeech.vercel.app/',
+    },
+    {
+        name: 'hbdAoPresh',
+        desc: 'Happpu Birthday Wish to Ao Presh',
+        lang: ' JavaScript + CSS + Html',
+        link: 'https://hbd-aopresh.vercel.app',
+    },
+    {
+        name: 'askSolomon',
+        desc: 'A search engine prototype that wokrs like google',
+        lang: 'JavaScript + Css + Html',
+        link: 'https://asksolomon.vercel.app/',
+    },
+    {
+        name: 'Zeddpay(clone)',
+        desc: 'zeddpay V1 web clone',
+        lang: ' JavaScript + Css + Html',
+        link: 'https://zeddpay-clvne.vercel.app/',
+    },
+    {
+        name: 'Scrimm',
+        desc: 'Get to know everything about scrim.',
+        lang: 'Bootstrap + Html',
+        link: 'https://victorola-coder.github.io/scrimm/',
+    },
+    {
+        name: 'afrifanz',
+        desc: 'onlyfans for africans content creators',
+        lang: 'JavaScript + ReactJS + TailwindCss',
+        link: 'https://waitlist.afrifanz.com/',
+    },
+    {
+        name: 'showmeelove',
+        desc: 'buymeeacoffe for african creators',
+        lang: 'ReactJs + Tailwindcs',
+        link: 'https://showmeelove.com/',
+    },
+    {
+        name: 'luxstay',
+        desc: 'help the homeless get affordable house',
+        lang: 'ReactJs + TailwindCss',
+        link: 'https://luxstayy.vercel.app/',
+    },
+    {
+        name: 'TekArt',
+        desc: 'bridging the gap between the tech and art space',
+        lang: 'ReactJs + TailwindCss',
+        link: 'https://tekarthq.com/',
+    },
+]
