1bf63898954ed20f04f3a4e23a5a0bb382287ef2
Victor Olatunji <victoluolatunji@gmail.com>
2023-10-19 00:50:32 +0100

    [chore] rearraned sections

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -20,8 +20,8 @@ function Home() {
         <Navbar />
         <Hero />
         <Services />
-        <Skills />
         <Projects />
+        <Skills />
         <Contact />
         <Footer />
       </main>
