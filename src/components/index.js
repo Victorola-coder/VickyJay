7678d893f417b037bfec01e464fbfff060f08f17
Victor Olatunji <victoluolatunji@gmail.com>
2024-03-12 01:54:55 +0100

    deploy?

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,5 @@ export { default as Projects } from "./projects";
 export { default as Services } from "./services";
 export { default as Footer } from "./footer";
 export { default as Contact } from "./contact";
-export { default as Skills } from "./skills";
+export { default as Skills } from "./Skills";
 export { default as Resume } from "./resume";
