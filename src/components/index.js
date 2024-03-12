f773d861658e070f1623bf2abc96d19baebbfdf3
Victor Olatunji <victoluolatunji@gmail.com>
2024-03-12 01:41:48 +0100

    deploy?

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export { default as Hero } from "./Hero";
 export { default as Navbar } from "./Navbar";
 export { default as SEO } from "./Seo";
-export { default as Projects } from "./Projects";
+export { default as Projects } from "./projects";
 export { default as Services } from "./Services";
 export { default as Footer } from "./Footer";
 export { default as Contact } from "./Contact";
