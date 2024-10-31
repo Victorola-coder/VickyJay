f34f685c10765d5cd5cec2b661952bdf4ec80456
Victorola <victoluolatunji@gmail.com>
2024-11-01 00:32:27 +0100

    [octagon]

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,25 +1,41 @@
 import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
 import { Copyright, FbIcon, IgIcon, InIcon, TwIcon } from "./svgs";
 
 const Footer = () => {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <section className="mx-[30px] mt-[60px] pb-[20px] lg:pb-[50px] lg:mx-[76px] lg:mt-[150px]">
       {/* line */}
       <div className="lg:max-w-[13000px] lg:mx-[71px] h-[2px] mt-[63.37px] bg-[#0A1C20]"></div>
       <div className="mt-[63px] flex flex-col md:flex md:flex-row justify-between items-center ">
-        <div className="flex flex-row items-center gap- lg:gap-[4px]">
-          <span>
-            <Copyright className="size-[18px] md:size-[20px]" />
-          </span>
-          <p className={`text-[#FFFFFF99] md:text-xl text-lg font-medium`}>
-            Victorola
-            <span className="text-[#FFFFFF] text-[14px] lg:text-base">
-              {" "}
-              2021 -
-            </span>{" "}
-            <span className="text-[#FFFFFF] text-[14px] lg:text-base">
-              {new Date().getFullYear()}
+        <div className="flex flex-col">
+          <div className="flex flex-row items-center gap- lg:gap-[4px]">
+            <span>
+              <Copyright className="size-[18px] md:size-[20px]" />
             </span>
+            <p className={`text-[#FFFFFF99] md:text-xl text-lg font-medium`}>
+              Victorola
+              <span className="text-[#FFFFFF] text-[14px] lg:text-base">
+                {" "}
+                2021 -
+              </span>{" "}
+              <span className="text-[#FFFFFF] text-[14px] lg:text-base">
+                {new Date().getFullYear()}
+              </span>
+            </p>
+          </div>
+          <p className="text-[#FFFFFF99] transition-all duration-300 text-end text-sm lg:text-base mt-1">
+            {time}
           </p>
         </div>
         <div className="mt-[10px] md:mt-[65px] flex justify-start items-center gap-[20px] md:gap-[40px]">
@@ -79,7 +95,7 @@ const Footer = () => {
       </div>
 
       {/* Shout out */}
-      <div className="hidden flex_ flex-col gap-2 items-start justify-center text-center font-medium text-[#FFFFFF99] text-xs *:max-w-[320px] *:md:max-w-[992px] md:text-[18px]">
+      <div className="hidden  flex_ flex-col gap-2 items-start justify-center text-center font-medium text-[#FFFFFF99] text-xs *:max-w-[320px] *:md:max-w-[992px] md:text-[18px]">
         <h3>
           Inspiration -&nbsp;
           <a
